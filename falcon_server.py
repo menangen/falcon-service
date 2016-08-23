5ff9aefde5e98b6004661ce66e06e2b73a3f48a1
Andrey Menangen <menangen@gmail.com>
2016-08-23 22:34:24 +0300

    Enable file logging at request processing

--- falcon_server.py
+++ falcon_server.py
@@ -2,38 +2,63 @@
 # -*- coding: utf-8 -*-
 import falcon
 import json
+import sys
+
+from logbook import Logger, StreamHandler, FileHandler
 
 
 class FormResource:
 
     @staticmethod
     def on_post(request, response):
+        # Enable logging to File
+        log_to_file = FileHandler('logs/service.log')
+        log_to_file.push_application()
+
+        # Enable logbook logger Stream to output
+        #StreamHandler(sys.stdout).push_application()
+
+        log_request = Logger('FormResource:request.stream.read')
+        log_response = Logger('FormResource:response.body')
 
         if request.content_length in (None, 0):
             # Nothing to do
             return
 
         body = request.stream.read()
+        # Error in body (sending ajax error / bad connection)
         if not body:
+            log_request.warning("Empty request body!")
             raise falcon.HTTPBadRequest('Empty request body',
                                         'A valid JSON document is required.')
+        else:
+            # Normal body read
+            log_request.info(body)
 
-        request_data = json.loads(body.decode('utf-8'))
+            try:
+                request_data = json.loads(body.decode('utf-8'))
+                log_request.info(request_data)
 
-        try:
-            username = request_data["username"]
-            # print "time_zone params is %s" % type(timezone)
-        except:
-            username = ""
+                username = request_data.get("username", "")
+                # print "time_zone params is %s" % type(timezone)
+                log_request.info("Username is")
+                log_request.info()
 
-        """Handles GET requests"""
-        response.status = falcon.HTTP_200  # This is the default status
-        response.content_type = "application/json"
-        response.set_header('Access-Control-Allow-Origin', '*')
+            except:
+                log_request.warning("Not valid JSON!")
+                username = ""
 
-        response_data = {"status":  "ok", "username": username}
+            """Handles GET requests"""
+            response.status = falcon.HTTP_200  # This is the default status
+            response.content_type = "application/json"
+            response.set_header('Access-Control-Allow-Origin', '*')
 
-        response.body = json.dumps(response_data)
+            response_data = {"status":  "ok", "username": username}
+
+            response.body = json.dumps(response_data)
+
+            # Logging response
+            log_response.info(response_data)
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
